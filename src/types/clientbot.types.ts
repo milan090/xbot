587de87f237ba167dd74adad50fca72172cf6524
milan090 <milanmuhammed1@gmail.com>
2020-10-28 23:23:02 +0530

    fix issues

--- src/types/clientbot.types.ts
+++ src/types/clientbot.types.ts
@@ -89,10 +89,7 @@ export default class ClientBot extends Client {
       .setAuthor(this.user?.username.toUpperCase())
       .setTitle(title)
       .setURL("https://github.com/milan090/xbot")
-    
-    if (content) {
-      embed.setDescription(content);
-    }
+      .setDescription(content)
 
     if (fields) {
       embed.addFields(...fields);
@@ -100,7 +97,7 @@ export default class ClientBot extends Client {
 
     embed
       .setTimestamp()
-      .setFooter("Bot Author: milan090");
+      .setFooter("Bot Author: Milan/milan090/codingdeck", "https://i.imgur.com/wSTFkRM.png");
     return embed;
   }
 }
